f0632a3d5c559e8c29e654b2fd1256ad276e931e
wangyong <wb_wangyong02@meituan.com>
2022-04-25 19:39:26 +0800

    项目初始化

--- spring-jdbc-samples/src/main/java/com/kevin/jdbc/samples/service/UserService.java
+++ spring-jdbc-samples/src/main/java/com/kevin/jdbc/samples/service/UserService.java
@@ -1,12 +1,10 @@
 package com.kevin.jdbc.samples.service;
 
 import com.kevin.jdbc.samples.repository.domain.entity.UserEntity;
-import org.springframework.stereotype.Service;
 
 /**
  * @author kevin
  */
-@Service
 public interface UserService {
 
     /**
--- spring-jdbc-samples/src/main/java/com/kevin/jdbc/samples/service/impl/UserServiceImpl.java
+++ spring-jdbc-samples/src/main/java/com/kevin/jdbc/samples/service/impl/UserServiceImpl.java
@@ -3,18 +3,21 @@ package com.kevin.jdbc.samples.service.impl;
 import com.kevin.jdbc.samples.repository.domain.entity.UserEntity;
 import com.kevin.jdbc.samples.service.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author kevin
  */
-@Transactional(propagation = Propagation.REQUIRED)
+@Service
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 public class UserServiceImpl implements UserService {
 
     private JdbcTemplate jdbcTemplate;
 
 
+    @Override
     public void save(UserEntity user) {
 
     }
